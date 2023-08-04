8d9b19830a3afeb95ac89c96d8c2e5ac62701220
Praveenkumarramadass <125397580+Praveenkumarramadass@users.noreply.github.com>
2023-08-04 17:44:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const eyel = document.querySelector('.pupill');
+        const eyer = document.querySelector('.pupilr')
+        window.addEventListener('mousemove', (evt) => {
+            const x = -(window.innerWidth  - evt.pageX) / 1000;
+            const y = -(window.innerHeight  - evt.pageY) / 1000;
+            eyel.style.transform = `translateY(${y*60}px) translateX(${x*60}px)`;
+            eyer.style.transform = `translateY(${y*60}px) translateX(${x*60}px)`;
+            eyel.style.left = "80px"
+            eyer.style.left = "80px"
+            eyel.style.top = "40px"
+            eyer.style.top = "40px"
+            eyel.style.bottom = "40px"
+            eyer.style.bottom = "40px"
+        });
